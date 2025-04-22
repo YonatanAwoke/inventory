bfe82b6830ffc4b25c1b7516c2a458c566a35117
Yonatan Awoke <yonatanawoke@gmail.com>
2025-04-23 01:44:07 +0300

    cors fix production

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,12 +15,24 @@ const prisma = new PrismaClient(
   }
 );
 
+const allowedOrigins = [
+  "http://localhost:5173",                
+  "https://inventory-pi-tan.vercel.app"       
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,              
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
